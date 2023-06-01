302d9feb000955b70d8b753bbef2d0ec35bbf1dd
Davi Castro <davialmeida47jd@gmail.com>
2023-06-01 00:13:35 -0300

    fix(cadastro): criado verificação para cadastrar preço e colocado arquivo views.py no padrão pep-8

--- pharmadjango/remedios/views.py
+++ pharmadjango/remedios/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import redirect, render
 from .models import Remedio
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+import re
+
 
 def lista_medicamentos_func(request):
     """lista todos os medicamentos cadastrados
@@ -14,6 +16,7 @@ def lista_medicamentos_func(request):
     context = {'remedios': remedios}
     return render(request, 'remedios/view_func.html', context)
 
+
 def lista_medicamentos_admin(request):
     """lista todos os medicamentos cadastrados
 
@@ -24,6 +27,7 @@ def lista_medicamentos_admin(request):
     remedios = Remedio.objects.all()
     context = {'remedios': remedios}
     return render(request, 'remedios/view_admin.html', context)
+
 
 def lista_medicamentos_cliente(request):
     """lista todos os medicamentos cadastrados
@@ -42,14 +46,23 @@ def cadastra_medicamentos(request):
     Cadastra os medicamentos no banco de dados
 
     """
-    if request.method=="POST":
+    if request.method == "POST":
         nome = request.POST["nome"]
         preco = request.POST["preco"]
         tarja = request.POST["tarja"]
         receita = request.POST["receita"]
+
+        # Verifica se o preço é float
+        if preco != float:
+            preco = re.sub(r'[^0-9.,]', '', preco)
+            if ',' in preco:
+                preco = preco.replace(',', '.')
+            preco = float(preco)
+
         Remedio.objects.create(nome=nome, tarja=tarja, precisa_receita=receita, preco=preco)
-        
+
     return render(request, "remedios/cadastro.html")
+
 
 def logar(request):
 
@@ -57,21 +70,22 @@ def logar(request):
         username = request.POST["usuario"]
         passw = request.POST["senha"]
         user = authenticate(request, username=username, password=passw)
-    
+
         if user is not None:
             if username == "admin":
                 return redirect("/listamedicamentosadmin")
-            
+
             login(request, user)
             return redirect("/listamedicamentosfunc")
-    
+
         else:
             messages.success(request, ("Usuário ou senha incorretos"))
             return redirect("/login")
-    
+
     else:
 
         return render(request, "remedios/login.html", {})
+
 
 def deletar(request, remedio_id):
     remedio = Remedio.objects.get(pk=remedio_id)
@@ -79,9 +93,10 @@ def deletar(request, remedio_id):
 
     return redirect("/listamedicamentosfunc")
 
+
 def adicionar_carrinho(request, remedio_id):
     remedio = Remedio.objects.get(pk=remedio_id)
 
-    context = {"remedio" : remedio}
-    
+    context = {"remedio": remedio}
+
     return render(request, "remedios/carrinho.html", context)
